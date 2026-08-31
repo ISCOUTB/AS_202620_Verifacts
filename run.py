207b28decc21f660a7285bc0405d0c9bd6c9e244
PedroC1213 <166455619+PedroC1213@users.noreply.github.com>
2026-08-31 10:10:38 -0500

    Initialize Uvicorn server in run.py
    Add Uvicorn server initialization for FastAPI app

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 from app.api.routes import router
 from app.persistence.repository import initialize_database
@@ -14,3 +15,12 @@ app = FastAPI(
 initialize_database()
 
 app.include_router(router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=False,
+    )
